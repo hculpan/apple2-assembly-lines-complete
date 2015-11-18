10344655f3631b63409ab04134306c0921cf6662
Harry Culpan <hculpan@scor.com>
2015-11-18 13:43:04 -0500

    Restructing build so don't have to have copies of binary files all over the place; experimenting with graphics and keyboard

--- chapter-18/ch18.s
+++ chapter-18/ch18.s
@@ -8,9 +8,9 @@
         ORG $6000
 
 *
-PREAD   EQU $FB1E
-WAIT    EQU $FCA8
-PB0     EQU $C061
+KYBD    EQU $C000
+STROBE  EQU $C010
+
 HCOLOR  EQU $F6F0
 HGR     EQU $F3E2
 HPLOT   EQU $F457
@@ -22,14 +22,14 @@ SHNUM   EQU $F730
 DRAW    EQU $F601
 PTR     EQU $E8
 
-ENTRY   JMP E2
+ENTRY   JMP START
 TABLE   HEX 010004
         HEX 00123F
         HEX 20642D
         HEX 15361E
         HEX 0700
 
-E2      JSR HGR       ; CLR SCRN
+START   JSR HGR       ; CLR SCRN
         LDX #$03      ; WHITE = 3
         JSR HCOLOR
 
@@ -56,36 +56,8 @@ BORDER  LDA #$00      ; Y = 0
         LDY #$00      ; Y = 0
         JSR HLIN      ; HLIN TO 0,0
 
-SET     LDA #$03
-        STA PTR
-        LDA #$60
-        STA PTR+1     ; SET TABLE TO $6003
+WAIT    LDA KYBD      ; LOAD KEY INTO A
+        CMP #$80      ; CMP TO $80 - NO KEY
+        BCC WAIT      ; IF CARRY CLEAR, BRANCH TO WAIT
 
-READ    LDX #$00      ; PDL(0)
-        JSR PREAD
-        TYA
-        BNE R1
-        LDA #$01      ; FIX 0 -> 1
-R1      STA SCALE
-        LDA #$18
-        JSR WAIT
-        LDX #$01      ; PDL(1)
-        JSR PREAD
-        STY ROT
-        LDA #$18
-        JSR WAIT
-
-DSPLY   LDX #$8B
-        LDY #$00      ; X = 139
-        LDA #$4F      ; Y = 79
-        JSR HPOSN
-        LDX #$01      ; SHAPE #1
-        JSR SHNUM     ;FIND SHP ADDR
-        LDA ROT
-        JSR DRAW+4    ; USE SHNUM ENTRY PT
-
-CHK     LDA PB0
-        BMI E2        ; BUTTON PUSHED
-        BPL READ      ; NO PUSH
-
-        CHK
+        RTS
